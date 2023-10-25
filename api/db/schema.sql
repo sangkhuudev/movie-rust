23b54955f889d62c1772507070564b02f53a3d8c
Khuu Viet Sang <sangkhuudev@gmail.com>
2023-10-25 10:38:58 +0700

    Setup database

---
+++ api/db/schema.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS fimls (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL CONSTRAINT films_pkey PRIMARY KEY,
+    title text NOT NULL,
+    director text NOT NULL,
+    year smallint NOT NULL,
+    poster text NOT NULL,
+    created_at timestamp with time zone default CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone
+);
