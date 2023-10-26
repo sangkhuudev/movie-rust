c8ea131d5fd6bda5e763f2e600d246105d402cff
Khuu Viet Sang <sangkhuudev@gmail.com>
2023-10-26 14:41:10 +0700

    Add layout components

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,27 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
+mod components;
+
+use components::{Footer, Header};
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello Rust"
+        }
+        main {
+            class: "relative z-0 bg-blue-100 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
+            Header {}
+            section {
+                class: "md:container md:mx-auto md:py-8 flex-1",
+            }
+            Footer {}
+        }
+    })
+}
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default().module_prefix("frontend"));
+    dioxus_web::launch(App);
 }
